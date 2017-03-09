2f1bba6ba86cff3b1533a0d4a2725caf6f95978e
Prabhat Thapa <gorebro@gmail.com>
2017-03-09 17:38:40 +0545

    managed templates

--- client_labs/src/main.js
+++ client_labs/src/main.js
@@ -5,18 +5,33 @@ import VueResource from 'vue-resource'
 import VueRouter from 'vue-router'
 import App from './App'
 import DashboardPage from './pages/DashboardPage.vue'
+import LoginPage from './pages/LoginPage.vue'
 Vue.config.productionTip = false
 Vue.use(VueRouter)
 Vue.use(VueResource)
 
+Vue.component('app', App)
+
 const routes = [
-    {path: '/', component: App, name: 'home'},
-    {path: '/dashboard', component: DashboardPage, name: 'dashboard'}
+    {path: '/', component: LoginPage, name: 'home'},
+    {path: '/dashboard', component: DashboardPage, name: 'dashboard', meta: { requiresAuth: true }}
 ]
 
 const router = new VueRouter({
   mode: 'history',
   routes
+})
+
+router.beforeEach((to, from, next) => {
+  if (to.meta.requiresAuth) {
+    const authUser = JSON.parse(window.localStorage.getItem('authUser'))
+    if (authUser && authUser.access_token) {
+      next()
+    } else {
+      next({name: 'home'})
+    }
+  }
+  next()
 })
 
 new Vue({
---
+++ client_labs/src/pages/LoginPage.vue
@@ -0,0 +1,79 @@
+<script>
+    import {loginUrl, getHeader, userUrl} from './../config'
+    import {clientId, clientSecret} from './../env'
+    export default {
+      data () {
+        return {
+          login: {
+            email: 'gorebro@gmail.com',
+            passord: 'secret'
+          }
+        }
+      },
+      methods: {
+        handleLoginFormSubmit () {
+          const postData = {
+            grant_type: 'password',
+            client_id: clientId,
+            client_secret: clientSecret,
+            username: this.login.email,
+            password: this.login.password,
+            scope: ''
+          }
+          const authUser = {}
+          this.$http.post(loginUrl, postData)
+             .then(response => {
+               if (response.status === 200) {
+                 console.log('OAUTH token', response)
+                 authUser.access_token = response.data.access_token
+                 authUser.refresh_token = response.data.refresh_token
+                 window.localStorage.setItem('authUser', JSON.stringify(authUser))
+                 this.$http.get(userUrl, {headers: getHeader()})
+                   .then(response => {
+                     console.log('user object', response)
+                     authUser.email = response.body.email
+                     authUser.name = response.body.name
+                     window.localStorage.setItem('authUser', JSON.stringify(authUser))
+                     this.$router.push({name: 'dashboard'})
+                   })
+               }
+             })
+        }
+      }
+    }
+</script>
+<template>
+    <div class="wrapper" id="login-wrapper">
+        <div class="login">
+            <div class="row">
+                <div class="col-md-6 col-md-push-3">
+                    <div class="panel panel-default">
+                        <div class="panel-heading"><strong>Login</strong></div>
+                        <div class="panel-body">
+                            <form v-on:submit.prevent="handleLoginFormSubmit()">
+                                <div class="form-group">
+                                    <label>Email Address</label>
+                                    <input type="text" class="form-control" placeholder="Enter email address"
+                                           v-model="login.email">
+                                </div>
+
+                                <div class="form-group">
+                                    <label>Password</label>
+                                    <input type="password" class="form-control" placeholder="Your Password Here"
+                                           v-model="login.password">
+                                </div>
+
+                                <button class="btn btn-primary">Login</button>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style lang="sass">
+    #login-wrapper
+
+</style>
